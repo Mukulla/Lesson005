e69415f0d8db43a14bb51c5d6f8ef34cf6c98885
Maxim <mbzozn@mail.ru>
2021-03-02 15:49:59 +0300

    First maked

--- Lesson005/Lesson005/Program.cs
+++ Lesson005/Lesson005/Program.cs
@@ -10,6 +10,51 @@ namespace Lesson005
     {
         static void Main(string[] args)
         {
+            int CountPars = 1;
+            string[] Denuntiatio = new string[]
+            {
+                "Запись произвольного текста в файл",
+                "Сумма всех чисел в строке",
+                "Определение времени года",
+                "Вычисление числа Фибоначчи"
+            };
+            //Console.WriteLine(System.Text.Encoding.Default.HeaderName);
+            //Цикл-обработчик каждого задания
+            for (int i = 0; i < CountPars; ++i)
+            {
+                //Вывод части и названия задания
+                Console.WriteLine($"Часть {i + 1}: {Denuntiatio[i]}");
+
+                switch (i)
+                {
+                    case 0:
+                        WriteToFile Writter = new WriteToFile();
+                        Writter.ReadText();
+                        Writter.WriteTextInFile("File001.txt");
+                        break;
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                }
+                if (i == 3)
+                {
+                    
+                    Console.WriteLine();
+                    Console.WriteLine("Все части пройдены");
+                    Console.WriteLine("Для выхода нажмите любую клавишу");
+                    Console.ReadKey();
+
+                    break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Для перехода к следующей части нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
---
+++ Lesson005/Lesson005/WriteToFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson005
+{
+    class WriteToFile
+    {
+        private string SomeText;
+        public void ReadText()
+        {
+            Console.WriteLine("Введите любой текст");
+            SomeText = Console.ReadLine();
+        }
+        public void WriteTextInFile(string Path001)
+        {
+            File.WriteAllText(Path001, SomeText);
+        }
+    }
+}
